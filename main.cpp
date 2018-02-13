e21e53325488d64276e4d1bb53aaa43f9eca9215
lixinrui706 <xli21@mail.csuchico.edu>
2018-02-12 17:41:47 -0800

    ask name

--- main.cpp
+++ main.cpp
@@ -1,4 +1,4 @@
-//Authors:
+//Authors:Xinrui Li
 #include<iostream>
 #include<string>
 
@@ -7,9 +7,14 @@ using namespace std;
 int main()
 {
   //declare variables
-
+  string lastname;
+  string firstname;
   //get user input
-
+  cout<<"What is your first name?\n";
+  cin>>firstname;
+  cout<<"what is your last name?\n";
+  cin>>lastname;
+  cout<<"Your first name is:"<<firstname<<" and your last name is:"<<lastname<<endl;
   //tell fortune
 
   return 0;
